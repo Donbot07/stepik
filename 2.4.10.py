3f127113b9af56df83bb8ce75d214c65e154ac5f
Donbot07 <pitertwin@gmail.com>
2023-05-17 15:08:30 +0300

    сумма, разность и произведение двух целых чисел

---
+++ 2.4.10.py
@@ -0,0 +1,15 @@
+a = int(input())
+b = int(input())
+
+add = a + b
+subtract = a - b
+mltplct = a * b
+print(a, '+', b, '=', add)
+print(a, '-', b, '=', subtract)
+print(a, '*', b, '=', mltplct)
+
+#лучшее решениеa = int(input()) 
+b = int(input()) 
+print(a, '+', b, '=', a + b)
+print(a, '-', b, '=', a - b)
+print(a, '*', b, '=', a * b)
