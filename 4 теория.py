b9eb82938978ecf9f48fc1b37b851ab8a584c89d
Donbot07 <pitertwin@gmail.com>
2023-05-21 15:07:22 +0300

    теория

---
+++ 4 теория.py
@@ -0,0 +1,40 @@
+a = int(input())
+a = a % 2 
+if a != 0:
+    print(a, 'нечет' )
+else:
+    print(a, 'чет')
+
+if i / 2:
+    print(i, 'чётное')
+else:
+    print(i, 'нечётное')
+
+if i // 2:
+    print(i, 'чётное')
+else:
+    print(i, 'нечётное')
+
+
+if i % 2 == 0:
+    print(i, 'чётное')
+else:
+    print(i, 'нечётное')
+
+
+if i // 2 == 0:
+    print(i, 'чётное')
+else:
+    print(i, 'нечётное')
+
+
+if i % 2 != 0:
+    print(i, 'нечётное')
+else:
+    print(i, 'чётное')
+
+    
+if i // 2 != 0:
+    print(i, 'нечётное')
+else:
+    print(i, 'чётное')
