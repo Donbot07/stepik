9c96d1e15ec80c22ebc90b4e51b6628b7deac843
Donbot07 <pitertwin@gmail.com>
2023-05-29 16:01:15 +0300

    done

--- Conditional operator_4/4.2.15_ход_короля.py
+++ Conditional operator_4/4.2.15_ход_короля.py
@@ -1,9 +1,8 @@
 a = int(input())
-a = int(input())
 b = int(input())
 c = int(input())
 d = int(input())
-if a == c or b == d:
+if (c == a + 1 or c == a - 1 or c == a) and (d == b + 1 or d == b - 1 or d == b):
     print('YES')
 else:
     print('NO')
